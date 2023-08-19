b1c743c9b5994d5d35d134edccd06d163bbc77f8
tquaddl <modellersamp@gmail.com>
2023-08-19 22:31:55 +0300

    Home screen layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:intl/intl.dart';
+import 'package:weather_app/consts/colors.dart';
 import 'consts/strings.dart';
+import 'consts/images.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,75 +55,193 @@ class WeatherApp extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            'MINSK'
-                .text
-                .fontFamily('poppins_bold')
-                .size(32)
-                .letterSpacing(3)
-                .make(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/weather/01d.png',
-                  width: 80,
-                  height: 80,
-                ),
-                RichText(
-                  text: TextSpan(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              'MINSK'
+                  .text
+                  .fontFamily('poppins_bold')
+                  .size(32)
+                  .letterSpacing(3)
+                  .make(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/weather/01d.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '37$degree',
+                          style: TextStyle(
+                            color: Vx.gray900,
+                            fontSize: 64,
+                            fontFamily: 'poppins',
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Sunny',
+                          style: TextStyle(
+                            color: Vx.gray700,
+                            letterSpacing: 3,
+                            fontSize: 14,
+                            fontFamily: 'poppins_light',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.expand_less_rounded,
+                      color: Vx.gray400,
+                    ),
+                    label: '41$degree'.text.make(),
+                  ),
+                  TextButton.icon(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.expand_more_rounded,
+                      color: Vx.gray400,
+                    ),
+                    label: '26$degree'.text.make(),
+                  ),
+                ],
+              ),
+              10.heightBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(3, (index) {
+                  var iconsList = [clouds, humidity, windspeed];
+                  var values = ['70%', '40%', '3.5 km/h'];
+                  return Column(
                     children: [
-                      TextSpan(
-                        text: '37$degree',
-                        style: TextStyle(
-                          color: Vx.gray900,
-                          fontSize: 64,
-                          fontFamily: 'poppins',
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Sunny',
-                        style: TextStyle(
-                          color: Vx.gray700,
-                          letterSpacing: 3,
-                          fontSize: 14,
-                          fontFamily: 'poppins_light',
-                        ),
-                      ),
+                      Image.asset(
+                        iconsList[index],
+                        width: 60,
+                        height: 60,
+                      )
+                          .box
+                          .gray200
+                          .padding(const EdgeInsets.all(8))
+                          .roundedSM
+                          .make(),
+                      10.heightBox,
+                      values[index].text.gray400.make(),
                     ],
-                  ),
+                  );
+                }),
+              ),
+              10.heightBox,
+              const Divider(),
+              10.heightBox,
+              SizedBox(
+                height: 150,
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(right: 4),
+                      decoration: BoxDecoration(
+                        color: cardColor,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          '${index + 1} AM'.text.gray200.make(),
+                          Image.asset(
+                            'assets/weather/09n.png',
+                            width: 80,
+                          ),
+                          '38$degree'.text.white.make(),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton.icon(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.expand_less_rounded,
-                    color: Vx.gray400,
+              ),
+              10.heightBox,
+              const Divider(),
+              10.heightBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  'Next 7 Days'.text.semiBold.size(16).make(),
+                  TextButton(
+                    onPressed: () {},
+                    child: 'View All'.text.make(),
                   ),
-                  label: '41$degree'.text.make(),
-                ),
-                TextButton.icon(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.expand_more_rounded,
-                    color: Vx.gray400,
-                  ),
-                  label: '26$degree'.text.make(),
-                ),
-              ],
-            ),
-            Row(
-              children: List.generate(3, (index) {
-                return Column();
-              }),
-            ),
-          ],
+                ],
+              ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 7,
+                itemBuilder: (BuildContext context, int index) {
+                  var day = DateFormat('EEEE')
+                      .format(DateTime.now().add(Duration(days: index + 1)));
+                  return Card(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(child: day.text.semiBold.make()),
+                          Expanded(
+                            child: TextButton.icon(
+                              onPressed: null,
+                              icon: Image.asset('assets/weather/50n.png',
+                                  width: 40),
+                              label: '26$degree'.text.gray800.make(),
+                            ),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: '37$degree',
+                                  style: TextStyle(
+                                    color: Vx.gray800,
+                                    fontFamily: 'poppins',
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: '26$degree',
+                                  style: TextStyle(
+                                    color: Vx.gray600,
+                                    fontFamily: 'poppins',
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
