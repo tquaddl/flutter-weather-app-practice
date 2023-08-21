4986d2ce1831e9ff275a99d4879d96f48447f562
tquaddl <modellersamp@gmail.com>
2023-08-21 15:06:58 +0300

    added OpenWeather API

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,1 +1,8 @@
-const String degree = "°";
+const String degree = '°';
+const String apiKey = '9ba56d6bc5427cf1627b630fe6638181';
+const String currentEndpoint =
+    'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+const String daysEndpoint =
+    'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+const String latitude = '53.893009';
+const String longitude = '27.567444';
