898bf707ffe74d439f548ec7169631c01b33830c
tquaddl <modellersamp@gmail.com>
2023-08-22 22:13:10 +0300

    fix TLS error and added http package

--- lib/services/apiServices.dart
+++ lib/services/apiServices.dart
@@ -1,1 +1,5 @@
+import 'package:weather_app/consts/strings.dart';
+import 'package:http/http.dart' as http;
 
+var link =
+    'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
